371a65f7905396f5fe00ca7b012cf0d2a72026b5
Ashish2796 <ashishgupta2796@gmail.com>
2020-09-14 17:47:08 +0530

    Understanding route snapshot and route observable

--- src/app/courses/lesson/lesson-detail.component.ts
+++ src/app/courses/lesson/lesson-detail.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {LessonDetail} from "../model/lesson-detail";
 import {Observable} from "rxjs";
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'lesson',
@@ -10,18 +11,26 @@ import {Observable} from "rxjs";
 })
 export class LessonDetailComponent implements OnInit {
 
-  // lesson$: Observable<LessonDetail>;
-  lesson: LessonDetail; 
-  constructor(private route : ActivatedRoute) {
+  lesson$: Observable<LessonDetail>;
+  //lesson: LessonDetail; 
+  constructor(private route : ActivatedRoute,private router : Router) {
 
     console.log("Created LessonDetailComponent...");
 
   }
 
   ngOnInit() {
-    this.lesson=this.route.snapshot.data["lesson"];
+    //this.lesson=this.route.snapshot.data["lesson"];
+    this.lesson$ =this.route.data.pipe(map(data=>data["lesson"]));
   }
 
+  previous(lesson:LessonDetail){
+    this.router.navigate(['lesson',lesson.seqNo-1],{relativeTo:this.route.parent});
+  }
+
+  next(lesson:LessonDetail){
+    this.router.navigate(['lesson',lesson.seqNo+1],{relativeTo:this.route.parent});
+  }
 
 
 }
